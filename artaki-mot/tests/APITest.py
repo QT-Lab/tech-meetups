14ff0abbcf17652830aaf78ede7b5d703b1300e0
darthspectrum <muradianator@gmail.com>
2022-02-28 17:30:40 +0400

    unittest vscode setup

--- artaki-mot/tests/APITest.py
+++ artaki-mot/tests/APITest.py
@@ -6,15 +6,12 @@ class APITest(unittest.TestCase):
 
     def test_pork_bbq_order(self):
         pork = {'type': 'pork'}
-
         resp = requests.get('http://localhost:8088/bbq', params=pork)
 
         self.assertEqual(resp.text, '4')
 
     def test_chicken_bbq_order(self):
-
         chicken = {'type': 'chicken'}
-
         resp = requests.get('http://localhost:8088/bbq', params=chicken)
 
         self.assertEqual(resp.text, '2324')
