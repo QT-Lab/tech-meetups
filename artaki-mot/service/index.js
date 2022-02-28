9027010f2ab07635b6f3bc64943c792451abfa1f
darthspectrum <muradianator@gmail.com>
2022-02-28 17:34:08 +0400

    refactoring (service): single quotes

--- artaki-mot/service/index.js
+++ artaki-mot/service/index.js
@@ -2,25 +2,25 @@ const express = require('express')
 
 const menu = {
   bbq: {
-    "pork": 1,
-    "chicken": 2324,
-    "beef": 3
+    'pork': 1,
+    'chicken': 2324,
+    'beef': 3
   },
   drinks: {
-    "tan": 11,
-    "cola": 22
+    'tan': 11,
+    'cola': 22
   }
 }
 
-const Artaki_mot = express()
+const ArtakiMot = express()
 
-Artaki_mot.get('/bbq', (req, res) => {
-  console.log(menu["bbq"][req.query.type])
-  res.json(menu["bbq"][req.query.type])
+ArtakiMot.get('/bbq', (req, res) => {
+  console.log(menu['bbq'][req.query.type])
+  res.json(menu['bbq'][req.query.type])
 })
 
-Artaki_mot.get('/drinks', (req, res) => {
-  res.json(menu["drinks"][req.query.type])
+ArtakiMot.get('/drinks', (req, res) => {
+  res.json(menu['drinks'][req.query.type])
 })
 
-Artaki_mot.listen(8088)
+ArtakiMot.listen(8088)
