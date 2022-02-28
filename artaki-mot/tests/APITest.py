dda701198192696a062d8df16ee4c9d76acbc9bd
darthspectrum <muradianator@gmail.com>
2022-02-28 17:03:48 +0400

    testing the pork and chicken order endpoints

---
+++ artaki-mot/tests/APITest.py
@@ -0,0 +1,24 @@
+import requests
+import unittest
+
+
+class APITest(unittest.TestCase):
+
+    def test_pork_bbq_order(self):
+        pork = {'type': 'pork'}
+
+        resp = requests.get('http://localhost:8088/bbq', params=pork)
+
+        self.assertEqual(resp.text, '4')
+
+    def test_chicken_bbq_order(self):
+
+        chicken = {'type': 'chicken'}
+
+        resp = requests.get('http://localhost:8088/bbq', params=chicken)
+
+        self.assertEqual(resp.text, '2324')
+
+
+if __name__ == '__main__':
+    unittest.main()
